f48100d0e3f0fe48f156cff5f502835b7e4f5ec1
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-09-02 03:12:45 +0600

    content added

--- js/trend-hunter.js
+++ js/trend-hunter.js
@@ -37,7 +37,9 @@ const displayPhones = (phones, dataLimit) =>{
                     <div class="card-body">
                       <h5 class="card-title">${phone.phone_name}</h5>
                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <button type="button" class="btn btn-outline-dark">More Details</button>
+
+                      <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">More Details</button>
+
                     </div>
                   </div>
                 </div>
@@ -58,7 +60,12 @@ document.getElementById('btn-search').addEventListener('click',function (){
   // start loader
   processSearch(10);
 })
-
+// search input field enter key handler
+document.getElementById('search-field'). addEventListener('keypress', function (e) {
+  if(e.key === 'Enter'){
+    processSearch(10);
+  }
+})
 const toggleLoader = isLoading =>{
   const loaderSection = document.getElementById('loader');
   if(isLoading){
@@ -72,4 +79,31 @@ document.getElementById('btn-show-all').addEventListener('click',function () {
   processSearch();
 })
 
-// loadPhones();
+const loadPhoneDetails = async id =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
+}
+const displayPhoneDetails = phone =>{
+  console.log(phone);
+  const modalTitle = document.getElementById('phoneModalLabel');
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.innerHTML = `
+  <h1 class =" text-center text-bg-dark">MainFeatures</h1>
+  <div class="container d-flex align-items-center gap-2">
+  <img src = "${phone.image}"> <br> </img>;
+  <ul class="list-group">
+  <li class="list-group-item"><span class= "fw-bold">Release Date: </span>${phone.releaseDate ? phone.releaseDate : 'No Release Date found'}</li>
+  <li class="list-group-item bg-white-50"><span class= "fw-bold">ChipSet: </span>${phone.mainFeatures.chipSet}</li>
+  <li class="list-group-item"><span class= "fw-bold">Diplay Size:</span> ${phone.mainFeatures.displaySize}</li>
+  <li class="list-group-item"><span class= "fw-bold">Memory: </span>${phone.mainFeatures.memory}</li>
+  <li class="list-group-item"><span class= "fw-bold">Storage:</span> ${phone.mainFeatures.storage}</li>
+</ul>;
+  </div>
+ 
+  `;
+}
+
+loadPhones('phone');
